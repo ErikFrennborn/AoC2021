16a3c79c14bc0e595e152f58f19be8f0f7210f01
ErikFrennborn <erikfre@student.chalmers.se>
2021-12-01 22:11:01 +0100

    Added testing input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import importlib
+import sys
 
 CURRENT_DAY = 1
 
@@ -10,14 +11,25 @@ def getInput(day):
     data_as_number = list(map(int, data_raw))
     return (data_raw, data_as_number)
 
-def main():
-    for i in range(CURRENT_DAY):
-        i =+1
-        today_code = importlib.import_module(f"day{i}")
-        (data_raw, data_as_number) = getInput(i)
-        print(f"Day {i}:")
-        today_code.main(data_raw,data_as_number)
+def main(argv):
+    match argv:
+        case ["-t", day, file]:
+            data_raw = []
+            with open(f"test_data/{file}") as inputfile:
+                data_raw = inputfile.readlines()
+
+            data_as_number = list(map(int, data_raw))
+
+            code = importlib.import_module(f"day{day}")
+            code.main(data_raw,data_as_number)
+        case _:
+            for i in range(CURRENT_DAY):
+                i =+1
+                code = importlib.import_module(f"day{i}")
+                (data_raw, data_as_number) = getInput(i)
+                print(f"Day {i}:")
+                code.main(data_raw,data_as_number)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
