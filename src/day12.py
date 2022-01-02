199a4ecd7db8c45964284e7eeadffed0f8a4f9fe
ErikFrennborn <erikfre@student.chalmers.se>
2022-01-02 14:13:46 +0100

    Solves day 12

--- src/day12.py
+++ src/day12.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+
 def createGraph(data):
     graph = {}
     unique_points = set([item for sublist in data for item in sublist])
@@ -11,9 +14,9 @@ def createGraph(data):
 
     return graph
 
-def exploreCave(graph, have_explored, current):
+def exploreCave(graph, have_explored, current, expection):
     paths = []
-    have_explored.add(current)
+    have_explored[current] += 1
 
     # base case
     if current == "end":
@@ -21,7 +24,8 @@ def exploreCave(graph, have_explored, current):
 
     for next_caves in graph[current]:
         # Don't got back to small caves
-        if next_caves.islower() and next_caves in have_explored:
+        limit = 2 if next_caves == expection else 1
+        if next_caves.islower() and have_explored[next_caves] == limit:
             continue
 
         # We should go back to start
@@ -29,7 +33,7 @@ def exploreCave(graph, have_explored, current):
             continue
 
         # Recursively search valid connected caves
-        for result in exploreCave(graph, have_explored.copy(), next_caves):
+        for result in exploreCave(graph, have_explored.copy(), next_caves, expection):
             result.append(current)
             paths.append(result)
 
@@ -37,14 +41,37 @@ def exploreCave(graph, have_explored, current):
 
 def problem1(data):
     graph = createGraph(data)
+
+    unique_points = set([item for sublist in data for item in sublist])
+    has_explored = {}
+    for point in unique_points:
+        has_explored[point] = 0
+
+
     # Recursively explore graph
-    return len(exploreCave(graph, set(), "start"))
+    return len(exploreCave(graph, has_explored, "start",None))
+
+def problem2(data):
+    graph = createGraph(data)
+    unique_points = set([item for sublist in data for item in sublist])
+    has_explored = {}
+    for point in unique_points:
+        has_explored[point] = 0
+
+
+    total_paths = set()
+    for point in unique_points:
+        if point == "start" or point == "end":
+            continue
+        total_paths = total_paths.union(set(list(map(lambda x:''.join(x),exploreCave(graph, has_explored.copy(), "start", point)))))
+    return len(total_paths)
+
 
 
 def main(data,_):
     parsedInput = list(map(lambda x: x.strip().split('-'), data))
     print(f"Problem 1: {problem1(parsedInput)}")
-    #  print(f"Problem 2: {problem2(parsedInput)}")
+    print(f"Problem 2: {problem2(parsedInput)}")
 
 def test_problem1_a():
     data = """dc-end
@@ -107,7 +134,7 @@ kj-sa
 kj-HN
 kj-dc""".split('\n')
     parsedInput = list(map(lambda x: x.strip().split('-'), data))
-    assert problem(parsedInput) == 103
+    assert problem2(parsedInput) == 103
 
 def test_problem2_c():
     data = """fs-end
